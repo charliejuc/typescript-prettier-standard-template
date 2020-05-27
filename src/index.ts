930653369f4384d7acc9a58df52f288174c39ce0
charliejuc <charliejuc@gmail.com>
2020-05-27 18:40:37 +0200

    Fixes module aliases.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-import 'source-map-support/register'
+import 'module-alias/register'
+import { hola } from '@/other'
 
 console.log('Hello world! :)')
+
+hola()
---
+++ src/other.ts
@@ -0,0 +1,3 @@
+export function hola(): void {
+    console.log('Hola')
+}
